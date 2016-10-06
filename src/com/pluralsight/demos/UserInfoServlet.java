573576e6e6f00191d3add9416e6cd349b64d6d00
Jerry Hu <hh201020@gmail.com>
2016-10-06 15:40:22 -0400

    http://localhost:8070/ServletApplicationsDemo/UserInfoServlet?firstName=Jerry&lastName=Hu to return a simple html page

---
+++ src/com/pluralsight/demos/UserInfoServlet.java
@@ -0,0 +1,54 @@
+package com.pluralsight.demos;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class UserInfoServlet
+ */
+@WebServlet("/UserInfoServlet")
+public class UserInfoServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public UserInfoServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+//		response.getWriter().append("Served at: ").append(request.getContextPath());
+		response.setContentType("text/html");
+		PrintWriter out = response.getWriter();
+		
+		String firstName = request.getParameter("firstName");
+		String lastName = request.getParameter("lastName");
+
+		out.println("<h3> getParameter() </h3>");
+		out.println("<div>");
+		out.println("<p> First Name: " + firstName + "</p>");
+		out.println("<p> Last Name: " + lastName + "</p>");
+		out.println("</div>");
+
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
