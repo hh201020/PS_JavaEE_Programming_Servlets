5db7d6ce1100b65d7bb3bbc428058caa1d4905d3
Jerry Hu <hh201020@gmail.com>
2016-10-07 10:37:49 -0400

    Http Status

---
+++ src/com/pluralsight/demos/StatusCodeDemo.java
@@ -0,0 +1,48 @@
+package com.pluralsight.demos;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class StatusCodeDemo
+ */
+@WebServlet("/StatusCodeDemo")
+public class StatusCodeDemo extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public StatusCodeDemo() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		String siteName = "http://www.pluralsight.com";
+		
+//		response.setStatus(HttpServletResponse.SC_MOVED_TEMPORARILY);
+//		response.setHeader("Location", siteName);
+		
+//		response.sendRedirect(siteName);
+		
+		response.sendError(401,"This Page Requires Authentication.");
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
